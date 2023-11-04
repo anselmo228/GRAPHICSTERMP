97834228f961414b80ae0262328945e0cee2d419
somi4219 <somi4219@gmail.com>
2023-11-04 09:46:00 +0900

    [ADD] 무당이 wasd 키로 움직이기

--- main/move.js
+++ main/move.js
@@ -1,4 +1,3 @@
-// 스크립트를 작성할 때 반드시 DOM이 로드된 후 실행되도록 주의하세요.
 document.addEventListener("DOMContentLoaded", function () {
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(
@@ -14,46 +13,80 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const loader = new THREE.GLTFLoader();
   let character;
+  let mixer;
 
-  loader.load("../model/mudang2.glb", (gltf) => {
+  let rotationSpeed = 0.09; // 회전 속도
+  let movementSpeed = 0.09; // 이동 속도
+
+  loader.load("../model/mudang.gltf", (gltf) => {
     character = gltf.scene;
     scene.add(character);
     character.position.set(0, 0, 0);
 
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
     directionalLight.position.set(1, 1, 1);
     character.add(directionalLight);
 
-    const directionalLight2 = new THREE.DirectionalLight(0xffffff, 1);
+    const directionalLight2 = new THREE.DirectionalLight(0xffffff, 2);
     directionalLight2.position.set(0, 0, 0);
     character.add(directionalLight2);
+
+    mixer = new THREE.AnimationMixer(gltf.scene);
+
+    const action = mixer.clipAction(gltf.animations[0]);
+    action.play();
 
     camera.position.z = 5;
 
     animate();
   });
 
+  let rotationAngle = 0; // 현재 회전 각도
+
+  document.addEventListener("keydown", (event) => {
+    switch (event.key) {
+      case "w":
+        // w 키 : 전진
+         mixer.timeScale = 2.0;
+        mixer.clipAction(mixer._actions[0].getClip()).play();
+        character.position.x += Math.sin(rotationAngle) * movementSpeed;
+        character.position.z += Math.cos(rotationAngle) * movementSpeed;
+        break;
+      case "s":
+        // s 키 : 후진
+        mixer.timeScale = -2.0; 
+        mixer.clipAction(mixer._actions[0].getClip()).play();
+        character.position.x -= Math.sin(rotationAngle) * movementSpeed;
+        character.position.z -= Math.cos(rotationAngle) * movementSpeed;
+        break;
+      case "a":
+        // a 키 : 좌측으로 회전
+        mixer.timeScale = 0; 
+        rotationAngle += rotationSpeed;
+        character.rotation.y = rotationAngle;
+        break;
+      case "d":
+        // d 키 : 우측으로 회전
+        mixer.timeScale = 0; 
+        rotationAngle -= rotationSpeed;
+        character.rotation.y = rotationAngle;
+        break;
+    }
+  });
+
+  document.addEventListener("keyup", (event) => {
+    // 키 안 눌렀을 때 멈추기
+    mixer.timeScale = 0;
+  });
+
   function animate() {
     requestAnimationFrame(animate);
 
-    const movementSpeed = 0.01;
+    // 모델의 애니메이션 업데이트
+    if (mixer) {
+      mixer.update(0.09); //애니메이션 재생 속도
+    }
 
-    document.addEventListener("keydown", (event) => {
-      switch (event.key) {
-        case "w":
-          character.position.z -= movementSpeed;
-          break;
-        case "s":
-          character.position.z += movementSpeed;
-          break;
-        case "a":
-          character.position.x -= movementSpeed;
-          break;
-        case "d":
-          character.position.x += movementSpeed;
-          break;
-      }
-    });
     renderer.render(scene, camera);
   }
 });
