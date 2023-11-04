b7015d671780c7d5726b1fe52572947107876a39
LeeJungKyun <naeric7@naver.com>
2023-11-04 18:17:49 +0900

    [ADD] 움직임 및 카메라

--- main/move.js
+++ main/move.js
@@ -8,6 +8,14 @@ document.addEventListener("DOMContentLoaded", function () {
   );
   const renderer = new THREE.WebGLRenderer({ antialias: true });
 
+  //바닥 생성
+  const groundGeometry = new THREE.PlaneGeometry(20, 20); // 바닥 크기 설정
+  const groundMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff }); // 바닥 색상 설정
+  const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+  ground.position.set(0, -3.5, 0);
+  ground.rotation.x = -Math.PI / 2; // 바닥을 수평으로 배치
+  scene.add(ground);
+
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.getElementById("scene-container").appendChild(renderer.domElement);
 
@@ -15,28 +23,33 @@ document.addEventListener("DOMContentLoaded", function () {
   let character;
   let mixer;
 
-  let rotationSpeed = 0.09; // 회전 속도
-  let movementSpeed = 0.09; // 이동 속도
+  let rotationSpeed = 0.2; // 회전 속도
+  let movementSpeed = 0.2; // 이동 속도
 
   loader.load("../model/mudang.gltf", (gltf) => {
     character = gltf.scene;
     scene.add(character);
+
     character.position.set(0, 0, 0);
 
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
     directionalLight.position.set(1, 1, 1);
     character.add(directionalLight);
 
-    const directionalLight2 = new THREE.DirectionalLight(0xffffff, 2);
-    directionalLight2.position.set(0, 0, 0);
+    const directionalLight2 = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight2.position.set(-1, -1, -1);
     character.add(directionalLight2);
+
+    const ambientLight = new THREE.AmbientLight(0x404040); // 회색 환경 조명
+    scene.add(ambientLight);
 
     mixer = new THREE.AnimationMixer(gltf.scene);
 
     const action = mixer.clipAction(gltf.animations[0]);
     action.play();
 
-    camera.position.z = 5;
+    camera.position.set(2, 5, 10);
+    camera.lookAt(character.position);
 
     animate();
   });
@@ -47,27 +60,27 @@ document.addEventListener("DOMContentLoaded", function () {
     switch (event.key) {
       case "w":
         // w 키 : 전진
-         mixer.timeScale = 2.0;
+        mixer.timeScale = 1.5;
         mixer.clipAction(mixer._actions[0].getClip()).play();
         character.position.x += Math.sin(rotationAngle) * movementSpeed;
         character.position.z += Math.cos(rotationAngle) * movementSpeed;
         break;
       case "s":
         // s 키 : 후진
-        mixer.timeScale = -2.0; 
+        mixer.timeScale = -1.5;
         mixer.clipAction(mixer._actions[0].getClip()).play();
         character.position.x -= Math.sin(rotationAngle) * movementSpeed;
         character.position.z -= Math.cos(rotationAngle) * movementSpeed;
         break;
       case "a":
         // a 키 : 좌측으로 회전
-        mixer.timeScale = 0; 
+        mixer.timeScale = 0;
         rotationAngle += rotationSpeed;
         character.rotation.y = rotationAngle;
         break;
       case "d":
         // d 키 : 우측으로 회전
-        mixer.timeScale = 0; 
+        mixer.timeScale = 0;
         rotationAngle -= rotationSpeed;
         character.rotation.y = rotationAngle;
         break;
@@ -82,9 +95,16 @@ document.addEventListener("DOMContentLoaded", function () {
   function animate() {
     requestAnimationFrame(animate);
 
+    //카메라 위치 업데이트
+    camera.position.x = character.position.x + 2; // x 위치를 캐릭터와 일치
+    camera.position.z = character.position.z + 10; // 뒤에서 캐릭터를 바라보도록 설정
+    camera.position.y = character.position.y + 5; // 머리 위에서 조금 떨어진 위치
+
+    camera.lookAt(character.position);
+
     // 모델의 애니메이션 업데이트
     if (mixer) {
-      mixer.update(0.09); //애니메이션 재생 속도
+      mixer.update(0.03); //애니메이션 재생 속도
     }
 
     renderer.render(scene, camera);
