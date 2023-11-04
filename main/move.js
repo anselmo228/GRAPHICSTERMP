423c721343322a20275fdb333189196067669d11
LeeJungKyun <naeric7@naver.com>
2023-11-04 20:07:32 +0900

    [ADD] 바닥이랑 건물 예시 코드
    너무 짜침

--- main/move.js
+++ main/move.js
@@ -6,15 +6,64 @@ document.addEventListener("DOMContentLoaded", function () {
     0.1,
     1000
   );
+
   const renderer = new THREE.WebGLRenderer({ antialias: true });
 
-  //바닥 생성
-  const groundGeometry = new THREE.PlaneGeometry(20, 20); // 바닥 크기 설정
-  const groundMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff }); // 바닥 색상 설정
-  const ground = new THREE.Mesh(groundGeometry, groundMaterial);
-  ground.position.set(0, -3.5, 0);
-  ground.rotation.x = -Math.PI / 2; // 바닥을 수평으로 배치
-  scene.add(ground);
+  //바닥 이미지
+  const textureLoader = new THREE.TextureLoader();
+  const floorTexture = textureLoader.load("../images/ground.png");
+
+  const medicalTexture = textureLoader.load("../images/medical.png");
+  const gachonTexture = textureLoader.load("../images/gachon.png");
+
+  floorTexture.wrapS = THREE.RepeatWrapping;
+  floorTexture.wrapT = THREE.RepeatWrapping;
+  floorTexture.repeat.x = 10;
+  floorTexture.repeat.y = 10;
+
+  //바닥 풀 Mesh
+  const meshes = [];
+  const spaceMesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(150, 150),
+    new THREE.MeshStandardMaterial({
+      map: floorTexture,
+    })
+  );
+  spaceMesh.name = "floor";
+  spaceMesh.position.set(0, -3.5, 0);
+  spaceMesh.rotation.x = -Math.PI / 2;
+  spaceMesh.receiveShadow = true;
+  scene.add(spaceMesh);
+  meshes.push(spaceMesh);
+
+  //메디컬 Mesh
+  const medicalMesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(6, 10),
+    new THREE.MeshStandardMaterial({
+      map: medicalTexture,
+      transparent: true,
+      opacity: 1.0,
+      color: "ffffff",
+    })
+  );
+  medicalMesh.name = "medical";
+  medicalMesh.position.set(3, -3.5, -3.5);
+  medicalMesh.rotation.x = -Math.PI / 2;
+  medicalMesh.receiveShadow = true;
+  scene.add(medicalMesh);
+
+  // //바닥 생성
+  // const groundGeometry = new THREE.PlaneGeometry(20, 20); // 바닥 크기 설정
+  // const groundMaterial = new THREE.MeshBasicMaterial({
+  //   color: 0xffffff,
+  //   roughness: 0.8,
+  //   metalness: 0.2,
+  // }); // 바닥 색상 설정
+  // const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+  // ground.position.set(0, -3.5, 0);
+  // ground.rotation.x = -Math.PI / 2; // 바닥을 수평으로 배치
+  // ground.receiveShadow = true; // 바닥에 그림자 투사
+  // scene.add(ground);
 
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.getElementById("scene-container").appendChild(renderer.domElement);
@@ -31,14 +80,35 @@ document.addEventListener("DOMContentLoaded", function () {
     scene.add(character);
 
     character.position.set(0, 0, 0);
+    character.castShadow = true;
+    character.receiveShadow = true;
 
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-    directionalLight.position.set(1, 1, 1);
-    character.add(directionalLight);
+    // const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    // directionalLight.position.set(1, 1, 1);
+    // character.add(directionalLight);
 
-    const directionalLight2 = new THREE.DirectionalLight(0xffffff, 1);
-    directionalLight2.position.set(-1, -1, -1);
-    character.add(directionalLight2);
+    // const directionalLight2 = new THREE.DirectionalLight(0xffffff, 1);
+    // directionalLight2.position.set(-1, -1, -1);
+    // character.add(directionalLight2);
+
+    const directionalLight = new THREE.DirectionalLight("white", 0.7);
+    const directionalLightOriginPosition = new THREE.Vector3(1, 1, 1);
+    directionalLight.position.x = directionalLightOriginPosition.x;
+    directionalLight.position.y = directionalLightOriginPosition.y;
+    directionalLight.position.z = directionalLightOriginPosition.z;
+    directionalLight.castShadow = true;
+
+    //그림자 퀄리티 설정
+    directionalLight.shadow.mapSize.width = 2048;
+    directionalLight.shadow.mapSize.height = 2048;
+    //그림자 범위
+    directionalLight.shadow.camera.left = -100;
+    directionalLight.shadow.camera.right = 100;
+    directionalLight.shadow.camera.top = 100;
+    directionalLight.shadow.camera.bottom = -100;
+    directionalLight.shadow.camera.near = 0.1;
+    directionalLight.shadow.camera.far = 100;
+    scene.add(directionalLight);
 
     const ambientLight = new THREE.AmbientLight(0x404040); // 회색 환경 조명
     scene.add(ambientLight);
