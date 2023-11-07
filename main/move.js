fdf6352a64aff15c6d141b575bd5f5a882746798
준희 <113875098+sungsil0624@users.noreply.github.com>
2023-11-08 01:27:05 +0900

    맵 요소 변경

--- main/move.js
+++ main/move.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const clock = new THREE.Clock();
+
   const scene = new THREE.Scene();
   scene.background = new THREE.Color(0x87ceeb); // 배경색 하늘색으로
   const camera = new THREE.PerspectiveCamera(
@@ -10,65 +12,35 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const renderer = new THREE.WebGLRenderer({ antialias: true });
 
-  const controls = new THREE.OrbitControls(camera, renderer.domElement);
-  controls.enableDamping = true;
-  controls.minDistance = 20;
-  controls.maxDistance = 800;
-  controls.update();
-
+  //바닥 이미지
   const textureLoader = new THREE.TextureLoader();
-   // 바닥 //
-  const skyMeterialArray = [];
-  const texture_ft = new THREE.TextureLoader().load("../background/ft.jpg");
-  const texture_bk = new THREE.TextureLoader().load("../background/bk.jpg");
-  const texture_up = new THREE.TextureLoader().load("../background/up.jpg");
-  const texture_dn = new THREE.TextureLoader().load("../background/dn.jpg");
-  const texture_rt = new THREE.TextureLoader().load("../background/rt.jpg");
-  const texture_lf = new THREE.TextureLoader().load("../background/lf.jpg");
+  const floorTexture = textureLoader.load("../images/ground.png");
+  const roadTexture = textureLoader.load("../images/road.png");
 
-  skyMeterialArray.push(
+  floorTexture.wrapS = THREE.RepeatWrapping;
+  floorTexture.wrapT = THREE.RepeatWrapping;
+  floorTexture.repeat.x = 10;
+  floorTexture.repeat.y = 10;
+
+  //바닥 풀 Mesh
+  const meshes = [];
+  const spaceMesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(150, 150),
     new THREE.MeshStandardMaterial({
-      map: texture_ft,
+      map: floorTexture,
     })
   );
-  skyMeterialArray.push(
-    new THREE.MeshStandardMaterial({
-      map: texture_bk,
-    })
-  );
-  skyMeterialArray.push(
-    new THREE.MeshStandardMaterial({
-      map: texture_up,
-    })
-  );
-  skyMeterialArray.push(
-    new THREE.MeshStandardMaterial({
-      map: texture_dn,
-    })
-  );
-  skyMeterialArray.push(
-    new THREE.MeshStandardMaterial({
-      map: texture_rt,
-    })
-  );
-  skyMeterialArray.push(
-    new THREE.MeshStandardMaterial({
-      map: texture_lf,
-    })
-  );
-  for (let i = 0; i < 6; i++) {
-    skyMeterialArray[i].side = THREE.BackSide;
-  }
-  const skyboxGeo = new THREE.BoxGeometry(100, 100, 100);
-  const sky = new THREE.Mesh(skyboxGeo, skyMeterialArray);
-  scene.add(sky);
+  spaceMesh.name = "floor";
+  spaceMesh.position.set(0, -3.5, 0);
+  spaceMesh.rotation.x = -Math.PI / 2;
+  spaceMesh.receiveShadow = true;
+  scene.add(spaceMesh);
+  meshes.push(spaceMesh);
 
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.getElementById("scene-container").appendChild(renderer.domElement);
 
-  // 길 //
-  const roadTexture = textureLoader.load("../images/road.png"); 
-  function createRoad(x, y, z) {
+  function createRoad(x, y, z, lotate) {
     const roadMesh = new THREE.Mesh(
       new THREE.PlaneGeometry(30, 15), // 크기 조절
       new THREE.MeshStandardMaterial({
@@ -78,18 +50,30 @@ document.addEventListener("DOMContentLoaded", function () {
       })
     );
     roadMesh.position.set(x, y, z);
+
+    if (lotate === 1) {
+      roadMesh.rotation.z = -Math.PI / 2;
+    }
+
     roadMesh.rotation.x = -Math.PI / 2;
+
     scene.add(roadMesh);
   }
+  createRoad(-30, -3.4, 0);
   createRoad(0, -3.4, 0);
   createRoad(30, -3.4, 0);
-  createRoad(60, -3.4, 0); // 이어붙여놨음
+  // 이어붙여놨음
+
+  createRoad(-4, -3.4, 15, 1);
+  createRoad(-4, -3.4, 45, 1);
+  createRoad(-4, -3.4, -15, 1);
+  createRoad(-4, -3.4, -45, 1);
 
   const loader = new THREE.GLTFLoader();
   let character;
   let mixer;
 
-  let rotationSpeed = 0.2; // 회전 속도
+  let rotationSpeed = 0.02; // 회전 속도
   let movementSpeed = 0.2; // 이동 속도
 
   // 무당이 //
@@ -97,7 +81,7 @@ document.addEventListener("DOMContentLoaded", function () {
     character = gltf.scene;
     scene.add(character);
 
-    character.position.set(0, -45, 0);
+    character.position.set(0, 0, -70);
     character.castShadow = true;
     character.receiveShadow = true;
 
@@ -135,34 +119,26 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // 통나무 //
-  loader.load("../model/log_stump/scene.gltf", (gltf) => {
-    const log_stump = gltf.scene.children[0];
-    scene.add(log_stump);
+  // loader.load("../model/log_stump/scene.gltf", (gltf) => {
+  //   const log_stump = gltf.scene.children[0];
+  //   scene.add(log_stump);
 
-    log_stump.position.set(5, -3.5, 5); //위치 설정 (x,y,z)
-    log_stump.scale.set(0.01, 0.01, 0.01); // 크기 조절
-    log_stump.castShadow = true;
-    log_stump.receiveShadow = true;
+  //   log_stump.position.set(5, 0, 5); //위치 설정 (x,y,z)
+  //   log_stump.scale.set(0.1, 0.1, 0.1); // 크기 조절
+  //   log_stump.castShadow = true;
+  //   log_stump.receiveShadow = true;
 
-    const light = new THREE.DirectionalLight(0xffffff, 1);
+  //   animate();
+  // });
 
-    light.position.set(5, 0, 5);
-
-    light.castShadow = true;
-
-    scene.add(light);
-
-    animate();
-  });
-
-  // 깃발 //
+  // 깃발
   loader.load("../model/flag/scene.gltf", (gltf) => {
     const flag = gltf.scene;
     scene.add(flag);
 
-    flag.position.set(6, 0, 5);
+    flag.position.set(-5, 0, -65);
 
-    flag.scale.set(1, 1, 1);
+    flag.scale.set(2, 2, 2);
 
     flag.castShadow = true;
     flag.receiveShadow = true;
@@ -175,8 +151,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const cube = gltf.scene;
     scene.add(cube);
 
-    cube.position.set(-5, 0, 5);
-    cube.scale.set(30, 30, 30);
+    cube.position.set(-50, 0, 0);
+    cube.scale.set(2, 2, 2);
 
     cube.castShadow = true;
     cube.receiveShadow = true;
@@ -184,13 +160,13 @@ document.addEventListener("DOMContentLoaded", function () {
     animate();
   });
 
-  // 바람개비 ( 바람개비 동산 ) //
+  // 바람개비 ( 바람개비 동산 )
   loader.load("../model/pinwheel/scene.gltf", (gltf) => {
     const pinwheel = gltf.scene;
     scene.add(pinwheel);
 
-    pinwheel.position.set(7, 0, 5);
-    pinwheel.scale.set(30, 30, 30);
+    pinwheel.position.set(0, 50, 0);
+    pinwheel.scale.set(100, 100, 100);
 
     pinwheel.castShadow = true;
     pinwheel.receiveShadow = true;
@@ -198,12 +174,12 @@ document.addEventListener("DOMContentLoaded", function () {
     animate();
   });
 
-  // 무한대 ( 가천관 ) //
+  // 무한대 ( 가천관 )
   loader.load("../model/infinity_loop/scene.gltf", (gltf) => {
     const infinity_loop = gltf.scene;
     scene.add(infinity_loop);
 
-    infinity_loop.position.set(2, 3.5, 0);
+    infinity_loop.position.set(50, 0, 0);
     infinity_loop.scale.set(0.01, 0.01, 0.01);
     infinity_loop.castShadow = true;
     infinity_loop.receiveShadow = true;
@@ -246,24 +222,56 @@ document.addEventListener("DOMContentLoaded", function () {
         break;
     }
   });
+
   document.addEventListener("keyup", (event) => {
     // 키 안 눌렀을 때 멈추기
     mixer.timeScale = 0;
   });
 
+  // function animate() {
+  //   requestAnimationFrame(animate);
+
+  //   //카메라 위치 업데이트
+  //   camera.position.x = character.position.x + 2; // x 위치를 캐릭터와 일치
+  //   camera.position.z = character.position.z + 10; // 뒤에서 캐릭터를 바라보도록 설정
+  //   camera.position.y = character.position.y + 5; // 머리 위에서 조금 떨어진 위치
+
+  //   camera.lookAt(character.position);
+
+  //   // 모델의 애니메이션 업데이트
+  //   if (mixer) {
+  //     mixer.update(0.03); //애니메이션 재생 속도
+  //   }
+
+  //   renderer.render(scene, camera);
+  // }
+
+  //3인칭 전면 시점
   function animate() {
     requestAnimationFrame(animate);
 
-    //카메라 위치 업데이트
-    camera.position.x = character.position.x + 2; // x 위치를 캐릭터와 일치
-    camera.position.z = character.position.z + 10; // 뒤에서 캐릭터를 바라보도록 설정
-    camera.position.y = character.position.y + 5; // 머리 위에서 조금 떨어진 위치
+    const delta = clock.getDelta();
+
+    // 카메라 위치 업데이트
+    const cameraDistance = 10; // 카메라와 캐릭터 사이의 거리
+    const cameraHeight = 8; // 카메라의 높이
+
+    const characterDirection = new THREE.Vector3(
+      -Math.sin(rotationAngle),
+      0,
+      -Math.cos(rotationAngle)
+    );
+    const cameraPosition = new THREE.Vector3()
+      .copy(character.position)
+      .addScaledVector(characterDirection, cameraDistance);
+    camera.position.copy(cameraPosition);
+    camera.position.y += cameraHeight; // 캐릭터 위치에서 높이 조정
 
     camera.lookAt(character.position);
 
     // 모델의 애니메이션 업데이트
     if (mixer) {
-      mixer.update(0.003); //애니메이션 재생 속도
+      mixer.update(delta / 2); // 애니메이션 재생 속도
     }
 
     renderer.render(scene, camera);
